12d30bb8205d4f8673897854c7d56ed6f36afba2
unknown <xiaoli.kou@raisound.com>
2021-12-01 17:01:34 +0800

    路径

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  publicPath: './'
+  // devServer: {
+  //   proxy: {
+  //     //laravel 中获取 csrf-token 的接口
+  //      '/wav': {
+  //           //将/api访问转换为target
+  //           target: 'http://localhost:8080',
+  //           //跨域请求携带cookie
+  //           changeOrigin: true,
+  //           //url 重写删除`/api`
+  //           pathRewrite: path => path.replace(/^\/wav/, ''),
+  //       },
+  //   },
+  // },
+}
